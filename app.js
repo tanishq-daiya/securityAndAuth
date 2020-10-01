9a6fc635a74c5282181ff7f11d47b7bc87075b11
Tanishq Daiya <daiyatanu@gmail.com>
2020-10-01 08:20:23 +0530

    md5 Encryption, 404 Not Found Page

--- app.js
+++ app.js
@@ -3,29 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 mongoose.connect('mongodb://localhost:27017/userDB', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
-});
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
+  password: String,
 });
 
 const User = new mongoose.model('User', userSchema);
@@ -45,7 +42,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password),
   });
 
   newUser.save((err) => {
@@ -59,23 +56,30 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
-  User.findOne({
-    email: username
-  }, (err, foundUser) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser.password === password) {
-        if (foundUser) {
-          res.render('secrets');
+  User.findOne(
+    {
+      email: username,
+    },
+    (err, foundUser) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser.password === password) {
+          if (foundUser) {
+            res.render('secrets');
+          }
         }
       }
     }
-  });
+  );
 });
 
-app.listen(3000, function() {
+app.get('*', (req, res) => {
+  res.status(404).render('404');
+});
+
+app.listen(3000, function () {
   console.log('Server Started on http://localhost:3000');
 });
